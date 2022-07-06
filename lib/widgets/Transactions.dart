3d50a9fa00a50ff928a21c80a8b56c9f81a94a36
mukku <mukku@DESKTOP-8QRM01T>
2022-07-07 00:14:45 +0530

    chart

--- lib/widgets/Transactions.dart
+++ lib/widgets/Transactions.dart
@@ -9,63 +9,85 @@ class Transactions extends StatelessWidget {
 
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        children: _usertransactions.map(
-          (tx) {
-            return Card(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tx.title,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.indigo,
+      child: _usertransactions.isEmpty
+          ? Column(
+              children: [
+                Text('No transactions registered Yet',
+                    style: Theme.of(context).textTheme.titleMedium),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  height: 200,
+                  child: Image.asset('assets/image/waiting.png',
+                      fit: BoxFit.cover),
+                )
+              ],
+            )
+          : Column(
+              children: _usertransactions.map(
+                (tx) {
+                  return Card(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              tx.title,
+                              textAlign: TextAlign.center,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium, /*TextStyle(
+                            color: Theme.of(context).primaryColor,
                             fontSize: 17,
                             fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        DateFormat('dd/MM/yyyy').format(tx.date),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Color.fromARGB(255, 131, 131, 135),
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    //width: double.infinity, this will create an error message
-                    margin: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.rectangle,
-                      border: Border.all(
-                        width: 2,
-                        color: Colors.indigo,
-                      ),
-                    ),
-                    padding: EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 20,
-                    ),
-                    child: Text(
-                      tx.amount.toStringAsFixed(2),
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          color: Colors.indigo,
+                      )*/
+                            ),
+                            Text(
+                              DateFormat('dd/MM/yyyy').format(tx.date),
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 131, 131, 135),
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          //width: double.infinity, this will create an error message
+                          margin: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            border: Border.all(
+                              width: 2,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 20,
+                          ),
+                          child: Text(
+                            tx.amount.toStringAsFixed(2),
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium /*TextStyle(
                           fontSize: 20,
-                          fontWeight: FontWeight.bold),
+                          fontWeight: FontWeight.bold)*/
+                            // color: Theme.of(context).primaryColor,
+
+                            ,
+                          ),
+                        )
+                      ],
                     ),
-                  )
-                ],
-              ),
-            );
-          },
-        ).toList(),
-      ),
+                  );
+                },
+              ).toList(),
+            ),
     );
   }
 }
--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,8 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:personall_app/models/transaction.dart';
+import 'package:intl/intl.dart';
 
 class chart extends StatelessWidget {
+  final List<Transaction> gettransactions;
+
+  chart(this.gettransactions);
+
+  List<Map<String, Object>> get recenttransaction {
+    return List.generate(
+      7,
+      (index) {
+        final weekday = DateTime.now().subtract(Duration(days: index));
+        double totalsum = 0.0;
+
+        for (int i = 0; i < gettransactions.length; i++) {
+          if (gettransactions[i].date.day == weekday.day &&
+              gettransactions[i].date.month == weekday.month &&
+              gettransactions[i].date.year == weekday.year) {
+            totalsum += gettransactions[i].amount;
+          }
+        }
+        return {"day": DateFormat.E().format(weekday), "amount": totalsum};
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    print(recenttransaction);
     return Container(
       width: double.infinity,
       height: 100,
@@ -10,7 +36,9 @@ class chart extends StatelessWidget {
       // color: Color.fromARGB(255, 122, 186, 238),
       child: Card(
         color: Color.fromARGB(255, 99, 179, 246),
-        child: Text("chart"),
+        child: Row(
+          children: [],
+        ),
       ),
     );
   }
